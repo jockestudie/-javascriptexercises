b6027963a19727200dfbae5e9596910c3b533c39
Jocke <persson.fristedt@comhem.se>
2018-01-30 08:15:12 +0100

    second

---
+++ dom-exercises/scripts/build.js
@@ -0,0 +1,6 @@
+let paragraph = document.createElement("h1");
+let paragraph = document.createElement("p");
+let htext = document.createTextNode("My text");
+let text = document.createTextNode("My text");
+header.appendChild(htext);
+paragraph.appendChild(text);
---
+++ dom-exercises/scripts/index.js
@@ -0,0 +1,36 @@
+/*var btn = document.getElementById("btn");
+
+var paragraph = document.getElementsByTagName('p')[0];
+
+
+btn.addEventListener("click",btnpress);
+function btnpress() {
+    paragraph.style.fontSize = '20px';
+    paragraph.style.color = 'red';
+
+    
+}*/
+
+/*let name = document.getElementById('name');
+let lname = document.getElementById('Lname');
+let btn = document.getElementById('btn');
+let listener = function modal() {
+    console.log(name.value);
+    console.log(lname.value);
+
+}
+
+btn.addEventListener('click', listener);*/
+
+
+
+/*let anchor = document.getElementById('anchor');
+let btn = document.getElementById('btn');
+let listener = function anc () {
+    console.log(anchor.getAttribute('id'));
+    console.log(anchor.getAttribute('hreflang'));
+    console.log(anchor.getAttribute('target'));
+    console.log(anchor.getAttribute('href'));
+}
+btn.addEventListener('click', listener);*/
+
