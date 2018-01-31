6f108e061bcf925d3a613f560c03b3d82be3059f
Jocke <persson.fristedt@comhem.se>
2018-01-31 14:50:06 +0100

    3

--- dom-exercises/scripts/index.js
+++ dom-exercises/scripts/index.js
@@ -34,3 +34,39 @@ let listener = function anc () {
 }
 btn.addEventListener('click', listener);*/
 
+
+/*function myFunction() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "NEW CELL1";
+    cell2.innerHTML = "NEW CELL2";
+}
+function myFunction2(){
+    document.getElementById("myTable").deleteRow(0);
+};*/
+/*function myFunction() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    document.getElementById("demo").innerHTML = "Width: " + w + "<br>Height: " + h;
+}*/
+/*var txt = "";
+txt += "<p>Total width/height: " + screen.width + "*" + screen.height + "</p>";
+txt += "<p>Available width/height: " + screen.availWidth + "*" + screen.availHeight + "</p>";
+
+
+document.getElementById("demo").innerHTML = txt;*/
+
+
+
+
+let item = document.querySelector('input[type="text"]');
+let value = document.querySelector('value');
+item.addEventListener('keyup', runEvent);
+
+function runEvent(e) {
+    console.log(item);
+    console.log(e.target.value);
+};
+
