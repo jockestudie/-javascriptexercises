4a63d1fcc56b6889171c19c6f950512cec6a4368
Jocke <persson.fristedt@comhem.se>
2018-02-01 13:31:14 +0100

    square root

---
+++ javascript/exercise-1/script.js
@@ -0,0 +1,2 @@
+let input = prompt("Please enter your name: ");
+console.log("Your name is: ", input);
---
+++ javascript/exercise-2/scripts/index.js
@@ -0,0 +1,40 @@
+document.getElementById("square").addEventListener("click", function () {
+    let num1 = prompt("Enter the number");
+    let number = parseInt(num1, 10);
+    let square;
+    let n1;
+    let isPositive = true;
+
+    if(number === 0){
+        document.getElementById("answer").innerHTML = "The square root of " + number
+        + "is 0."
+
+    }
+
+    else if (number < 0){
+        number = -number;
+        isPositive = false;
+    }
+
+    square = number / 2;
+    while(Math.abs(n1 - square) !== 0) {
+        n1 = square;
+        square = (n1 + (number/n1)) /2;
+    }
+
+    if(isPositive){
+        document.getElementById("answer").innerHTML = "The square root of  " + number
+        + " are " + "+" + square + " and -" + square ;
+    }
+
+    else {
+        document.getElementById("answer").innerHTML = "The square root of  " + number
+            + " are " + "+" + square + " i and -" + square + "i";
+    }
+
+});
+
+
+
+    //let answer = Math.sqrt(input);
+
