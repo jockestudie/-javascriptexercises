e1bee9725efe37429d68b98c0e952745add174b9
Jocke <persson.fristedt@comhem.se>
2018-02-05 15:36:10 +0100

    exercise9

---
+++ javascript/exercise-9/index.js
@@ -0,0 +1,18 @@
+let sum2 = 100;
+
+function Addition() {
+    let FirstNumber = document.form.txtFirstNumber.value;
+    let sum2 = document.form.sum2.value;
+    let sum = Number(FirstNumber) + Number(sum2);
+    alert("Your Result is: " + sum);
+    //document.getElementById("summ").innerHTML = "The New saldo are " + sum;
+}
+function Subtraction()
+{
+    let FirstNumber = document.form.txtFirstNumber.value;
+    let sum2 = document.form.sum2.value;
+    let sum = Number(FirstNumber) ; Number(sum2);
+    alert("Your Result is: " + sum)
+    // document.getElementById("summ").innerHTML = "The New saldo are " + sum;
+}
+
